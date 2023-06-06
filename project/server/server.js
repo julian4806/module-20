4e3c5feb198bcfb8a8bfc9450fd9e523fc075328
Julian van Husen <210549@student.glu.nl>
2023-06-06 11:34:58 +0200

    Plan v an Aanpak aangepast en oefenen Express

---
+++ project/server/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/subscribers");
+
+app.listen(3000, () => {
+  console.log("server start");
+});
