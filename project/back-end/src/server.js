87cb0e1a227f6cf2e0568554cc07ffc7c21d84b0
Julian van Husen <210549@student.glu.nl>
2023-06-12 11:51:58 +0200

    nodejs backend af!

--- project/back-end/src/server.js
+++ project/back-end/src/server.js
@@ -1,23 +1,42 @@
-import express from "express"
-import { cartItems, products } from './temp-data'
+import express from "express";
+import {
+  cartItems as cartItemsRaw,
+  products as ProductsRaw,
+} from "./temp-data";
 
-const app = express()
+let cartItems = cartItemsRaw;
+let products = ProductsRaw;
 
-app.get('/hello', (req, res) => {
-    res.send('Hey!sssssssssssssssssssss')
-})
-app.get('/products', (req, res) => {
-    res.json(products)
-})
-app.get('/cart', (req, res) => {
-    res.json(cartItems)
-})
-app.get('/products/:productId', (req, res) => {
-    const productId = req.params.productId
-    const product = products.find(product => product.id === productId)
-    res.json(product)
-})
+const app = express();
+app.use(express.json());
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+function populateCartIds(ids) {
+  return ids.map((id) => products.find((product) => product.id === id));
+}
+
+app.get("/cart", (req, res) => {
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
+});
+
+app.post("/cart", (req, res) => {
+  const productId = req.body.id;
+  cartItems.push(productId);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
+});
+
+app.delete("/cart/:productId", (req, res) => {
+  const productId = req.params.productId;
+  cartItems = cartItems.filter((id) => id !== productId);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
+});
 
 app.listen(8000, () => {
-    console.log('server is listening on port 8000')
-})
+  console.log("server is listening on port 8000");
+});
--- project/back-end/src/temp-data.js
+++ project/back-end/src/temp-data.js
@@ -6,58 +6,49 @@
 // import mediumBottle from './assets/medium-bottle.webp';
 // import boldBottle from './assets/bold-bottle.webp';
 
-export const cartItems = [{
-    id: '234',
-    name: 'Lemon',
-    price: '$26.00',
-    // imageName: lemonBottle,
-}, {
-    id: '345',
-    name: 'Garlic',
-    price: '$26.00',
-    // imageName: garlicBottle,
-}, {
-    id: '456',
-    name: 'Rosemary',
-    price: '$26.00',
-    // imageName: rosemaryBottle,
-},
-]
+export const cartItems = ["234", "345"];
 
-
-export const products = [{
-    id: '234',
-    name: 'Lemon',
-    price: '$26.00',
+export const products = [
+  {
+    id: "234",
+    name: "Lemon",
+    price: "$26.00",
     // imageName: lemonBottle,
-}, {
-    id: '345',
-    name: 'Garlic',
-    price: '$26.00',
+  },
+  {
+    id: "345",
+    name: "Garlic",
+    price: "$26.00",
     // imageName: garlicBottle,
-}, {
-    id: '456',
-    name: 'Rosemary',
-    price: '$26.00',
+  },
+  {
+    id: "456",
+    name: "Rosemary",
+    price: "$26.00",
     // imageName: rosemaryBottle,
-}, {
-    id: '567',
-    name: 'Chili',
-    price: '$26.00',
+  },
+  {
+    id: "567",
+    name: "Chili",
+    price: "$26.00",
     // imageName: chiliBottle,
-}, {
-    id: '678',
-    name: 'Delicate',
-    price: '$24.50',
+  },
+  {
+    id: "678",
+    name: "Delicate",
+    price: "$24.50",
     // imageName: delicateBottle,
-}, {
-    id: '789',
-    name: 'Medium',
-    price: '$24.50',
+  },
+  {
+    id: "789",
+    name: "Medium",
+    price: "$24.50",
     // imageName: mediumBottle,
-}, {
-    id: '890',
-    name: 'Bold',
-    price: '$24.50',
+  },
+  {
+    id: "890",
+    name: "Bold",
+    price: "$24.50",
     // imageName: boldBottle,
-}];
+  },
+];
