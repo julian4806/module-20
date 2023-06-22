c499ca78fcfc280674a284015beaaeaace4e9f68
Julian4806 <210549@student.glu.nl>
2023-06-22 18:30:25 +0200

    Firebase geprobeerd te koppelen

--- project/front-end/src/main.js
+++ project/front-end/src/main.js
@@ -1,12 +1,26 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./main.css"
+import 'dotenv/config'
 
 import * as VueRouter from "vue-router";
 import ShoppingCartPage from "./pages/ShoppingCartPage.vue";
 import ProductsPage from "./pages/ProductsPage.vue";
 import ProductDetailPage from "./pages/ProductDetailPage.vue";
 import NotFoundPage from "./pages/NotFoundPage.vue";
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 createApp(App)
   .use(
--- project/front-end/src/pages/ProductDetailPage.vue
+++ project/front-end/src/pages/ProductDetailPage.vue
@@ -10,6 +10,7 @@
         Add to Cart
       </button>
       <button class="grey-button" v-else>Item is already in Cart</button>
+      <button class="sign-in" @click="signIn">Sign in to ad to Cart</button>
     </div>
   </div>
   <div v-else>
@@ -17,6 +18,13 @@
   </div>
 </template>
 <script>
+import {
+  getAuth,
+  sendSignInLinkToEmail,
+  isSignInWithEmailLink,
+  signInWithEmailLink,
+} from "firebase";
+
 import axios from "axios";
 import NotFoundPage from "./NotFoundPage.vue";
 
@@ -42,9 +50,29 @@ export default {
       });
       alert("Succesfully added item to cart!");
     },
+
+    async signIn() {
+      const email = prompt("Please enter your email to sign in");
+      const auth = getAuth();
+      const actionCodeSettings = {
+        url: `http://localhost:8080/products/${this.$route.params.product.productId}`,
+        handleCodeInApp: true,
+      };
+      await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+      alert("A login link was sent to your email");
+      window.localStorage.setItem("emailForSignIn", email);
+    },
   },
   components: { NotFoundPage },
   async created() {
+    const auth = getAuth();
+    if (isSignInWithEmailLink(auth, window.location.href)) {
+      const email = window.localStorage.getItem("emailForSignIn");
+      await signInWithEmailLink(auth, email, window.location.href);
+      alert("Successfully signed in!");
+      window.localStorage.removeItem("emailForSignIn");
+    }
+
     const response = await axios.get(
       `/api/products/${this.$route.params.productId}`
     );
